7a093a3564569994d790a364ac69544efaf461b0
Aleksandr Sokolov <a@isokolov.com>
2025-04-30 00:47:37 +0200

    Initial commit

---
+++ app/src/app/app.config.ts
@@ -0,0 +1,22 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideFileRouter, requestContextInterceptor } from '@analogjs/router';
+
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideFileRouter(),
+    provideClientHydration(),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([requestContextInterceptor])
+    ),
+
+  ],
+};
---
+++ app/vite.config.ts
@@ -0,0 +1,47 @@
+/// <reference types="vitest" />
+
+import analog from '@analogjs/platform';
+import { defineConfig, Plugin } from 'vite';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+// @ts-expect-error @tailwindcss/vite currently uses mts. TypeScript is complaining this, but it works as expected.
+import tailwindcss from '@tailwindcss/vite';
+
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    root: __dirname,
+    cacheDir: `../node_modules/.vite`,
+    
+    build: {
+      outDir: '../dist/./app/client',
+      reportCompressedSize: true,
+      target: ['es2020'],
+    },
+    server: {
+      fs: {
+        allow: ['.'],
+      },
+    },
+    plugins: [
+      
+      tailwindcss(),
+      
+      
+      analog(),
+      
+      nxViteTsPaths(),
+    ],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['src/test-setup.ts'],
+      include: ['**/*.spec.ts'],
+      reporters: ['default'],
+    },
+    define: {
+      'import.meta.vitest': mode !== 'production',
+    },
+  };
+});

9521fd7a186218126f20f0057780d39abcae4cbc
Aleksandr Sokolov <a@isokolov.com>
2025-05-01 11:34:53 +0200

    fix metadata

--- app/src/app/pages/(home).page.ts
+++ app/src/app/pages/(home).page.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
+import {RouteMeta} from '@analogjs/router';
 
+export const routeMeta: RouteMeta = {
+  title: 'Aleksandr Sokolov | Frontend Developer',
+  meta: [
+    {
+      name: 'description',
+      content: 'Frontend Developer based in Poland. I specialize in building modern web applications using Angular and TypeScript.',
+    },
+    {
+      name: 'author',
+      content: 'Aleksandr Sokolov',
+    },
+    {
+      property: 'og:title',
+      content: 'Aleksandr Sokolov | Frontend Developer',
+    },
+    {
+      property: 'og:description',
+      content: 'Frontend Developer based in Poland. I specialize in building modern web applications using Angular and TypeScript.',
+    }
+  ],
+};
 
 @Component({
   selector: 'app-home',
   template: `
     <section class="max-w-[75%] mx-auto px-4 py-16">
-      <h1 class="text-6xl font-bold mb-6">Hello.</h1>
-      <h2 class="text-2xl font-semibold mb-6">My name is Aleksandr Sokolov.</h2>
+      <p class="text-6xl font-bold mb-6">Hello.</p>
+      <h1 class="text-2xl font-semibold mb-6">My name is Aleksandr Sokolov.</h1>
 
-      <p class="text-gray-700 leading-relaxed mb-4">
+      <p class="text leading-relaxed mb-4">
         I’m a frontend developer based in Poland. I specialize in building modern web applications using
         <span class="text-black font-medium">Angular</span>, <span class="text-black font-medium">TypeScript</span>,
         <span class="text-black font-medium">RxJS</span>, and <span class="text-black font-medium">NgRx</span>.
@@ -16,12 +38,12 @@ import { Component } from '@angular/core';
         solutions that support the digital transformation of learning.
       </p>
 
-      <p class="text-gray-700 leading-relaxed mb-4">
+      <p class="text leading-relaxed mb-4">
         I have over 10 years of experience in creating user interfaces, optimizing performance, and implementing
         architectural solutions in large-scale projects.
       </p>
 
-      <p class="text-gray-700 leading-relaxed">
+      <p class="text leading-relaxed">
         I'm passionate about technology, always eager to learn, and enjoy working on projects that bring real value.
         Feel free to connect with me on
         <a href="https://www.linkedin.com/in/soko1off/" class="linkedin">LinkedIn</a> or
@@ -31,11 +53,12 @@ import { Component } from '@angular/core';
     </section>
   `,
   styles: `
-    p {
+    p.text {
       font-size: 1.3rem;
       font-weight: 300;
       line-height: 1.4;
       max-width: 28em;
+      color: #364153;
     }
     a {
       text-decoration: none;
